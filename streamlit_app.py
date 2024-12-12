e8bc0b94f82816532473bd84289e668642c43a1a
Jesus Ruiz Toledo <jesusruiz2829@gmail.com>
2024-12-12 09:14:03 +0100

    primer commit

---
+++ streamlit_app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+from streamlit_echarts import st_echarts
+
+df = pd.read_csv('./data/employees.csv')
+
+st.title('Employee Data')
+
+st.write(df)
+st_echarts(options={
+    'xAxis': {
+        'type': 'category',
+        'data': df['salary'].tolist()
+    },
+    'yAxis': {
+        'type': 'value'
+    },
+    'series': [{
+        'data': df['Salary'].tolist(),
+        'type': 'bar'
+    }],
+    'title': {'text': 'Employee Salary'}
+})
